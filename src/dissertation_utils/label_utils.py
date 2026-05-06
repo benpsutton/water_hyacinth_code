f18074c407769931c6e229b3731102fd6b248d6f
benpsutton <benpsutton@gmail.com>
2026-05-06 16:10:52 +0100

    Updates to target image selection

--- src/dissertation_utils/label_utils.py
+++ src/dissertation_utils/label_utils.py
@@ -57,7 +57,7 @@ def create_s1_list_for_location(location: str, site_fp: str | Path):
     return s1_fc
 
 
-def git_s2_s1_matching_dates(location: str, site_fp: str | Path, cloud_perc: int = 10):
+def get_s2_s1_matching_dates(location: str, site_fp: str | Path, cloud_perc: int = 10):
     s1_fc = create_s1_list_for_location(location=location, site_fp=site_fp)
     s2_fc = create_s2_list_for_location(
         location=location,
